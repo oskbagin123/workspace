1de693212b031ac292942efe5e6ff862df9f949a
Oskar Baginski <oskbag@gmail.com>
2020-05-22 16:12:39 +0200

    Doing some stuff from chapter 2

---
+++ Rust/NoStarchTut/guessing-game/target/debug/deps/guessing_game-30253910f3811c05.d
@@ -0,0 +1,5 @@
+/home/oskar/workspace/Rust/NoStarchTut/guessing-game/target/debug/deps/guessing_game-30253910f3811c05: src/main.rs
+
+/home/oskar/workspace/Rust/NoStarchTut/guessing-game/target/debug/deps/guessing_game-30253910f3811c05.d: src/main.rs
+
+src/main.rs:
