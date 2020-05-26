c0941a9bea5f82af910a120179bcb7203001780d
Oskar Baginski <oskbag@gmail.com>
2020-05-26 23:00:01 +0200

    Adding random number.

--- Rust/NoStarchTut/guessing-game/src/main.rs
+++ Rust/NoStarchTut/guessing-game/src/main.rs
@@ -1,8 +1,12 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
     println!("input ur guess:");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is {}.", secret_number);
 
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Faield to read line");
